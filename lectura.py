a15e7a8638bf152042ca1fa39dde73515deff0e9
Haopeng Lin <haopeng138@gmail.com>
2021-10-23 16:02:26 +0200

    input

--- lectura.py
+++ lectura.py
@@ -1,5 +1,10 @@
-f = open("text.txt","r")
+def get_file_and_write(file_name):
+    f = open(file_name,"r")
+    g = open("musica.scd","w")
+    g.write(f.read())
 
-g = open("musica.scd","w")
 
-g.write(f.read())
+file = input("Fichero de entrada \n")
+get_file_and_write(file) 
+
+ 
